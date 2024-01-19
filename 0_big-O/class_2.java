ec14210f6c0ef55c00e9931e83a5d8b0c89162a8
bugrahan karamollaoglu <bugrahankaramollaoglu@gmail.com>
2024-01-20 01:32:52 +0300

    asd

---
+++ 0_big-O/class_2.java
@@ -0,0 +1,27 @@
+
+class class_2 {
+	/*
+	 * complexity for this is
+	 * O(len_a + len_b)
+	 */
+	public static void fun1() {
+		for (int a : arr_a) {
+			System.err.println(a);
+		}
+		for (int b : arr_b) {
+			System.err.println(b);
+		}
+	}
+
+	/*
+	 * complexity for this is
+	 * O(len_a * len_b)
+	 */
+	public static void fun2() {
+		for (int a : arr_a) {
+			for (int b : arr_b) {
+				System.err.println(a + " " + b);
+			}
+		}
+	}
+}
